4d52481fbc1d2722faa317fd8aa59a8c464fceb5
Ismail Hossain <md02.06ismail2021@gmail.com>
2024-07-14 22:26:39 +0600

    new file:   Java/primenumber.class 	new file:   Java/primenumber.java

---
+++ Java/primenumber.java
@@ -0,0 +1,24 @@
+public class primenumber {
+
+    public static void main(String[] args) {
+        for(int i =1;i<=100;i++){
+            if (isprime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static boolean isprime(int num){
+        if (num<=1) {
+            return false;
+        
+        }
+        for (int i =2;i<=num/2;i++){
+            if (num % i ==0){
+                return false;
+            }
+        }
+
+        return true;
+    
+    }
+}
