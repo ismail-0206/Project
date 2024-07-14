480b0492c5c07630d5618b9f3a7cbe7afe480e0f
Ismail Hossain <md02.06ismail2021@gmail.com>
2024-07-14 23:34:19 +0600

    modified:   Java/primenumber.java

--- Java/primenumber.java
+++ Java/primenumber.java
@@ -7,17 +7,17 @@ public class primenumber {
             }
         }
     }
-public static boolean isprime(int num){
-    if (num<=1) {
-        return false;
-    
-    }
-    for (int i =2;i<=num/2;i++){
-        if (num % i ==0){
-            return false;
+        public static boolean isprime(int num){
+            if (num<=1) {
+                return false;
+            
+            }
+            for (int i =2;i<=num/2;i++){
+                if (num % i ==0){
+                    return false;
+                }
+            }
+            return true;
+        
         }
-    }
-    return true;
-
-}
 }
